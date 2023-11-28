50058cd2f3dbaeed32d37ec0e83823c07aade511
Miguel Ortega <mick.ortega.g@gmail.com>
2023-11-27 20:54:12 -0600

    Cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,30 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector(".name"); // se coloca . , para llamar a las clases
+const $b = document.querySelector(".blog"); // se coloca . , para llamar a las clases
+const $l = document.querySelector(".location"); // se coloca . , para llamar a las clases
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//Se agrego async, porque se esta utilizando await
+async function displayUser(username) {
+  $n.textContent = "Cargando...";
+
+  //Se agrego el bloque ded try...catch para el manejo de errores
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    //Cambio en de backticks por comillas
+    $n.textContent = data.name || "Nombre no disponible";
+    $b.textContent = data.blog || "Blog no disponible";
+    $l.textContent = data.location || "Ubicación no disponible";
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.log("OH NO!");
+  console.error(err);
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("stolinski");
